1c5f5fdefeff56adb4cfb1c21e0089581bf88f3c
Marcin Kowal <polluma@gmail.com>
2015-03-08 20:55:57 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+__author__ = 'Marcin'
+
+
+def simple_func(x):
+    print(x)
+    return
+
+
+class Koko:
+
+    def __init__(self,x):
+        self.number = x
+
+    def print_number(self):
+        print(self.number)
+
+if __name__ == '__main__':
+    print('Hello')
+    x = input('Type number\n')
+    print(x)
+    print(type(x))
